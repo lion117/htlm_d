71a3d8d53287b3466852e1b3c3a02fed09d6e921
hesiyuan <hesiyuan117@qq.com>
2018-04-02 11:51:59 +0800

    add files

---
+++ httpsrvdemo.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import redirect
+
+
+app = Flask(__name__)
+
+@app.route('/jump',methods=['GET','POST'])
+def Redirect():
+    lUrl = "https://www.bing.com/search?q=%s"%GetLastday(0)
+    return  redirect(lUrl)
+
+	
+	
+@app.route('/',methods=['GET','POST'])
+def Index():
+	lStr = u"hello world"
+	return lStr
+	
+
+def GetLastday(tDays= 1):
+    import datetime
+    today = datetime.date.today()
+    oneday = datetime.timedelta(days=tDays)
+    iLastday = today - oneday
+    return  iLastday.strftime("%Y-%m-%d")
+
+	
+	
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True,port=8080)
